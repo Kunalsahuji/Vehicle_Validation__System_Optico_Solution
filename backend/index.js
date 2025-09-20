28febfb3493cc12bc8927389c51ba20e09a6d74e
user.name : "ksahu0103@gmail.com <ksahu0103@gmail.com>
2025-09-20 17:53:56 +0530

    In backend, connect database with server

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const cors = require('cors');
-
+const PORT = process.env.PORT || 5000;
 // CORS configuration
 app.use(cors({
   origin: process.env.CORS_ORIGIN,
@@ -10,11 +10,14 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
+// Database connection
+const db = require('./config/db');
+db();
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
- 
+
 // Sample route
 
 
@@ -22,6 +25,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(PORT, () => {
+  console.log('Server is running on port :', PORT);
 });
