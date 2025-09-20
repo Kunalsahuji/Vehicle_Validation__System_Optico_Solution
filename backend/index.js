33d70900c8f588f67d61b2440b4ca18f950cd371
user.name : "ksahu0103@gmail.com <ksahu0103@gmail.com>
2025-09-20 19:18:36 +0530

    Refactor index.js to improve route organization and update CORS configuration; add morgan for logging

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,19 @@ const app = express();
 require('dotenv').config();
 const cors = require('cors');
 const PORT = process.env.PORT || 5000;
+
+// Import routes
+const authRoutes = require('./routes/authRoutes');
+const adminRoutes = require('./routes/adminRoutes');
+const vehicleRoutes = require('./routes/vehicleRoutes');
+
 // CORS configuration
-app.use(cors({
-  origin: process.env.CORS_ORIGIN,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors
+  ({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }));
 
 // Database connection
 const db = require('./config/db');
@@ -18,7 +25,14 @@ db();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Sample route
+//morgan setup
+const morgan = require('morgan');
+app.use(morgan('dev'));
+
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/admins', adminRoutes);
+app.use('/api/vehicles', vehicleRoutes);
 
 
 app.get('/', (req, res) => {
